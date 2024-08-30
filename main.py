ded2ccc6b9969f1e243d00a6042bd6172a4bd754
Spring <Springsquash93@gmail.com>
2024-08-30 18:12:34 -0400

    Handle keyboard interrupt

--- main.py
+++ main.py
@@ -12,36 +12,34 @@ def main():
     recorder = Recorder()
     model = joblib.load("class.pkl")
 
-    recorder.start()
+    try:
+        recorder.start()
 
-    while True:
-        frame = recorder.get_frame()
+        while True:
+            frame = recorder.get_frame()
 
-        if frame is not None:
-            print("[+] Captured frame")
+            if frame is not None:
+                print("[+] Captured frame")
 
-            frame = np.array(frame)
+                frame = np.array(frame)
 
-            if frame.dtype != np.float32 and frame.dtype != np.float64:
-                print("[+] Converted to float32.")
-                frame = frame.astype(np.float32, order="C") / 32768.0
+                if frame.dtype != np.float32 and frame.dtype != np.float64:
+                    frame = frame.astype(np.float32, order="C") / 32768.0
 
-            features = extract_features_from_memory(frame, recorder.sampling_rate * 1000).reshape(1, -1)
-            prediction = model.predict(features)
+                features = extract_features_from_memory(frame, recorder.sampling_rate * 1000).reshape(1, -1)
+                prediction = model.predict(features)
 
-            result = "Doorbell" if prediction[0] == 1 else "No Doorbell"
-            print(result)
+                result = "Doorbell" if prediction[0] == 1 else "No Doorbell"
+                print(result)
 
-            if prediction[0] == 1:
-                timestamp = time.strftime("%Y%m%d-%H%M%S")
-                save_audio_to_file(f"data\\verify\\manual_check-{timestamp}.wav", frame)
-                print("Saved file for manual verification.")
+                if prediction[0] == 1:
+                    timestamp = time.strftime("%Y%m%d-%H%M%S")
+                    save_audio_to_file(f"data\\verify\\manual_check-{timestamp}.wav", frame)
+                    print("Saved file for manual verification.")
 
-        else:
-            # print("[!] No audio frame capture")
-            pass
-
-    recorder.stop()
+    except KeyboardInterrupt:
+        recorder.stop()
+        print("[!] Stopping")
 
 if __name__ == "__main__":
     main()
